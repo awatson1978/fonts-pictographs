c6e41f1559fef932786599967061e848dcec0720
Abigail Watson <awatson1978@gmail.com>
2013-10-01 18:51:30 -0400

    removed register_extension api call

--- package.js
+++ package.js
@@ -2,17 +2,6 @@ Package.describe({
   summary: "fonts-barcode is a Meteorite package for adding the 3 of 9 Barcode font to Meteor apps."
 });
 
-Package.register_extension(
-  "otf", function (bundle, source_path, serve_path, where) {
-    bundle.add_resource({
-      type: "static",
-      path: '/fonts/' + serve_path.split('/').pop(),
-      source_file: source_path,
-      where: where
-    });
-  }
-);
-
 Package.on_use(function (api) {
     api.add_files('fonts/ModernPictographs.otf', "client");
     api.add_files('fonts-pictographs.css', "client");
