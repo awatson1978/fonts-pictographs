bebd1941ea705ddaea8782f842e03d60033c7459
Abigail Watson <awatson1978@gmail.com>
2014-11-27 02:20:02 -0500

    updated package urls; bump to 1.0.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'fonts-pictographs is a Meteorite package for adding the 3 of 9 Barcode font to Meteor apps.',
-  version: '1.0.0',
+  version: '1.0.1',
   name: 'awatson1978:fonts-pictographs',
   git: 'http://github.com/awatson1978/fonts-pictographs.git'
 });
